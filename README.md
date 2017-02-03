2a6321ff6593baca3d738a53a04e7581203b4c7c
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-02-03 23:11:55 +0100

    Add some new modules

--- README.md
+++ README.md
@@ -10,6 +10,11 @@ Arch Linux ARM setup script to obtain a full RPI with Automatic Reverse Proxy wi
 - [hermanzdosilovic/rpi-docker-letsencrypt-nginx-proxy-companion](https://hub.docker.com/r/hermanzdosilovic/rpi-docker-letsencrypt-nginx-proxy-companion/) as automatic Let's Encrypt provisioner ( official companion docker for braingamer/nginx-proxy-arm )
 - [apache-nginx-referral-spam-blacklist](https://github.com/Stevie-Ray/apache-nginx-referral-spam-blacklist) preloaded for every host
 
+## Modules
+- [UI for Docker](https://github.com/kevana/ui-for-docker) ( [ui_for_docker.sh](modules/ui_for_docker.sh) )
+- [Portainer](https://github.com/portainer/portainer) ( [portainer.sh](modules/portainer.sh) )
+- [DNS Server](https://github.com/julianxhokaxhiu/docker-powerdns) ( [dns_server.sh](modules/dns_server.sh) )
+
 ## Requirements
 A clean Arch Linux ARM install with SSH capability as root user ( or any user which has sudo powers ).
 
@@ -19,6 +24,13 @@ wget https://github.com/julianxhokaxhiu/rpi-powered-by-docker/archive/master.zip
 unzip master.zip && cd rpi-powered-by-docker-master
 find ./ -name "*.sh" -exec chmod +x {} \;
 ./install.sh
+```
+
+## Module setup
+Edit the configuration variables to fit your needs, inside every module, then
+```bash
+./modules/<module_name>.sh
+# example ./modules/dns_server.sh
 ```
 
 ## Disclaimer
---
+++ modules/dns_server.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Configuration variables
+DNSSERVER_DOMAIN="dns.lan"
+DNSSERVER_NAME="dns-server"
+LETSENCRYPT_EMAIL="foo@bar.mail"
+CUSTOM_DNS="8.8.8.8;8.8.4.4;[2001:4860:4860::8888];[2001:4860:4860::8844]"
+API_KEY=""
+CRONTAB_TIME="0 10 * * *"
+ENABLE_ADBLOCK=false
+
+# Prepare the DNS Server data folder
+echo ">> Creating /srv/data/$DNSSERVER_DOMAIN folder..."
+mkdir -p "/srv/data/$DNSSERVER_DOMAIN" &>/dev/null
+
+# Enable IPv6 support in Docker
+echo ">> Enabling IPv6 support in your Docker service..."
+if ! [ -f "/etc/docker/daemon.json" ]; then
+  echo "{\"ipv6\": true,\"fixed-cidr-v6\": \"fd00:dead:beef::/48\"}" > /etc/docker/daemon.json
+else
+  echo -e "\nIMPORTANT! ADD THIS MANUALLY TO YOUR '/etc/docker/daemon.json' FILE:\n\n{\"ipv6\": true,\"fixed-cidr-v6\": \"fd00:dead:beef::/48\"}\n\nTO ENABLE IPV6 SUPPORT IN DOCKER!\n"
+fi
+
+# Provide IPv6 NAT feature
+echo ">> Enabling IPv6 NAT..."
+docker run \
+    -d \
+    --name="ipv6nat" \
+    --restart=always \
+    --privileged \
+    --net=host \
+    -v /lib/modules:/lib/modules:ro \
+    -v /var/run/docker.sock:/var/run/docker.sock:ro \
+    robbertkl/ipv6nat:armhf &>/dev/null
+
+# Install DNS Server
+echo ">> Running DNS Server..."
+docker run \
+    -d \
+    --name="$DNSSERVER_DOMAIN" \
+    --restart=always \
+    -e "API_KEY=$API_KEY" \
+    -e "CUSTOM_DNS=$CUSTOM_DNS" \
+    -e "CRONTAB_TIME=$CRONTAB_TIME" \
+    -e "ENABLE_ADBLOCK=$ENABLE_ADBLOCK" \
+    -e "VIRTUAL_HOST=$DNSSERVER_DOMAIN" \
+    -e "VIRTUAL_PORT=8080" \
+    -e "LETSENCRYPT_HOST=$DNSSERVER_DOMAIN" \
+    -e "LETSENCRYPT_EMAIL=$LETSENCRYPT_EMAIL" \
+    -p 53:53 \
+    -p 53:53/udp \
+    -v "/srv/data/$DNSSERVER_DOMAIN:/srv/data" \
+    julianxhokaxhiu/docker-powerdns:armhf &>/dev/null
+
+# Wait until the docker is up and running
+echo -n ">> Waiting for DNS Server to start..."
+while [ ! $(docker top $DNSSERVER_DOMAIN &>/dev/null && echo $?) ]
+do
+    echo -n "."
+    sleep 0.5
+done
+echo "started!"
+
+# Print friendly done message
+echo "-----------------------------------------------------"
+echo "All right! Everything seems to be installed correctly. Remember to restart the Docker service to have everything working properly. Have a nice day!"
+echo ">> URL: https://${DNSSERVER_DOMAIN}/"
+echo ">> DNS: TCP/UDP on Port 53"
+echo "-----------------------------------------------------"
