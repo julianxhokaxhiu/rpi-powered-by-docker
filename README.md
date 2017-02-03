9fbb9e988c178821f85d3e9f3a148fa9a5bec877
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-02-03 11:16:55 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # rpi-powered-by-docker
-Arch Linux ARM setup script to obtain a full RPI with Automatic Reverse Proxy and more without pain
+Arch Linux ARM setup script to obtain a full RPI with Automatic Reverse Proxy without pain
+
+## Stack
+- IPv4/IPv6 support ( Dual Stack )
+- [Git](https://git-scm.com/)
+- [Docker](https://www.docker.com/)
+- [talmai/rpi-watchtower](https://hub.docker.com/r/talmai/rpi-watchtower) as the Docker auto-update manager
+- [braingamer/nginx-proxy-arm](https://hub.docker.com/r/braingamer/nginx-proxy-arm/) as Reverse Proxy
+- [hermanzdosilovic/rpi-docker-letsencrypt-nginx-proxy-companion](https://hub.docker.com/r/hermanzdosilovic/rpi-docker-letsencrypt-nginx-proxy-companion/) as automatic Let's Encrypt provisioner ( official companion docker for braingamer/nginx-proxy-arm )
+- [apache-nginx-referral-spam-blacklist](https://github.com/Stevie-Ray/apache-nginx-referral-spam-blacklist) preloaded for every host
+
+## Requirements
+A clean Arch Linux ARM install with SSH capability as root user ( or any user which has sudo powers ).
+
+## Installation
+```bash
+wget https://github.com/julianxhokaxhiu/rpi-powered-by-docker/archive/master.zip
+unzip master.zip && cd rpi-powered-by-docker-master
+find ./ -name "*.sh" -exec chmod +x {} \;
+./install.sh
+```
+
+## Disclaimer
+- The mapping of the domains to the Host IP is considered done already externally to this project ( through DNS Server or statically inside your `hosts` file )
---
+++ install.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# Install Docker
+echo ">> Installing required packages..."
+yes '' | pacman -Sy --noprogressbar --noconfirm --needed docker git wget &>/dev/null
+
+# Enable and Start docker host service
+echo ">> Enabling docker service..."
+systemctl enable docker.service &>/dev/null
+systemctl start docker.service &>/dev/null
+
+# Prepare the Automatic Reverse proxy manager certs folder
+echo ">> Creating /srv/certs folder..."
+mkdir -p /srv/certs &>/dev/null
+
+# Prepare the generic git projects container folder
+echo ">> Creating /srv/git folder..."
+mkdir -p /srv/git &>/dev/null
+
+# Clone the referrals spam protection
+echo ">> Cloning the referrals spam protection project into /srv/git/apache-nginx-referral-spam-blacklist..."
+git clone https://github.com/Stevie-Ray/apache-nginx-referral-spam-blacklist.git /srv/git/apache-nginx-referral-spam-blacklist &>/dev/null
+
+# Prepare the generic template container folder
+echo ">> Creating /srv/tmpl folder..."
+mkdir -p /srv/tmpl &>/dev/null
+
+# Get the new nginx template for the reverse proxy
+echo ">> Getting the nginx template for the reverse proxy which includes referrals spam protection..."
+wget -q https://raw.githubusercontent.com/julianxhokaxhiu/rpi-powered-by-docker/master/nginx.tmpl -O /srv/tmpl/nginx.tmpl &>/dev/null
+
+# Get the nginx proxy custom configuration
+echo ">> Getting the nginx custom proxy configuration..."
+wget -q https://raw.githubusercontent.com/julianxhokaxhiu/rpi-powered-by-docker/master/proxy.conf -O /srv/tmpl/proxy.conf &>/dev/null
+
+# Prepare the generic vhost container folder
+echo ">> Creating /srv/vhost folder..."
+mkdir -p /srv/vhost &>/dev/null
+
+# Install Automatic Reverse proxy manager
+echo ">> Running Reverse Proxy docker..."
+docker run \
+    --restart=always \
+    --name=docker-auto-reverse-proxy \
+    -d \
+    -p 80:80 \
+    -p 443:443 \
+    -v /usr/share/nginx/html \
+    -v /srv/certs:/etc/nginx/certs:ro \
+    -v /srv/tmpl/nginx.tmpl:/app/nginx.tmpl:ro \
+    -v /srv/tmpl/proxy.conf:/etc/nginx/proxy.conf:ro \
+    -v /srv/vhost/:/etc/nginx/vhost.d \
+    -v /srv/git/apache-nginx-referral-spam-blacklist/referral-spam.conf:/etc/nginx/referral-spam.conf:ro \
+    -v /var/run/docker.sock:/tmp/docker.sock:ro \
+    braingamer/nginx-proxy-arm &>/dev/null
+
+# Install the Let's Encrypt Reverse Proxy companion
+echo ">> Running Let's Encrypt Reverse Proxy companion..."
+docker run \
+  --restart=always \
+  --name=docker-auto-reverse-proxy-companion \
+  -d \
+  -v /srv/certs:/etc/nginx/certs:rw \
+  --volumes-from docker-auto-reverse-proxy \
+  -v /var/run/docker.sock:/var/run/docker.sock:ro \
+  hermanzdosilovic/rpi-docker-letsencrypt-nginx-proxy-companion &>/dev/null
+
+# Autoupdate Dockers from time to time and cleanup old images
+echo ">> Running Docker Auto-Update manager..."
+docker run \
+  --restart=always \
+  --name=docker-autoupdate \
+  -d \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  talmai/rpi-watchtower --cleanup &>/dev/null
+
+# Print friendly done message
+echo "-----------------------------------------------------"
+echo "All right! Everything seems to be installed correctly. It's truly suggested to reboot now your system to get everything up and running."
+echo "Have a nice day!"
+echo "-----------------------------------------------------"
